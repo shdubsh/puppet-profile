2c3cd02e01d2409251967cd64a2751a87f6ba7bf
Cole White <cwhite@wikimedia.org>
2018-09-19 16:47:01 -0600

    initial commit

---
+++ manifests/puppetmaster.pp
@@ -0,0 +1,12 @@
+class profile::puppetmaster {
+  class { 'puppetserver':
+    config => {
+      'java_args'     => {
+        'xms'         => '512m',
+        'xmx'         => '1g',
+        'maxpermsize' => '512m',
+        'tmpdir'      => '/tmp',
+      }
+    }
+  }
+}
